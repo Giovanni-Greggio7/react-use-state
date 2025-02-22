aa41f09a49fc3ff1a59cfd2e0fa80b226dc1652a
Giovanni-Greggio7 <giovannigreggio2@gmail.com>
2025-02-22 19:59:30 +0100

    Exercise over

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,18 +1,29 @@
+import { useState } from 'react';
 const Main = (props) => {
 
     const { languages } = props
 
     return (
-        <div>
+        <div className="d-flex">
             {
                 languages.map((element) => {
+
+                    const [isOpen, setIsOpen] = useState(false);
+
+                    const handleLinguaggio = () => setIsOpen(!isOpen);
 
                     const { id, title, description } = element;
 
                     return (
-                        <div key={id}>
-                            <h3>{title}</h3>
-                            <p>{description}</p>
+                        <div key={id} className=''>
+                            <button onClick={handleLinguaggio}>{title}</button>
+                            <div className='langDescription'>
+                                {isOpen && <div>
+                                    <h2>{title}</h2>
+                                    {description}
+                                </div>}
+                            </div>
+
                         </div>
                     )
                 })
